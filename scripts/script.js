000da6eb6270588dcd17ea331c0e4942c7bda789
asifrahi9 <asifahemmed0@gmail.com>
2024-03-14 12:03:41 +0600

    categories added

--- scripts/script.js
+++ scripts/script.js
@@ -8,9 +8,9 @@ const loadAllButton = async () => {
 };
 
 // Load All Data From API
-const loadAllData = async () => {
+const loadAllData = async (id) => {
   const res = await fetch(
-    "https://openapi.programming-hero.com/api/videos/category/1000"
+    `https://openapi.programming-hero.com/api/videos/category/${id}`
   );
   const data = await res.json();
   showAllData(data.data);
@@ -21,7 +21,10 @@ const showButton = (data) => {
   const btnContainer = document.getElementById("btn-container");
   data.forEach((item) => {
     const btn = document.createElement("button");
-    btn.classList = "btn text-black border-0 bg-gray-300";
+    btn.classList = "btn text-black border-0 bg-gray-300 hover:bg-red-400 hover:text-white";
+    btn.addEventListener("click", () =>{
+      loadAllData(item.category_id);
+    });
     btn.innerHTML = item.category;
     btnContainer.appendChild(btn);
   });
@@ -31,28 +34,45 @@ const showButton = (data) => {
 const showAllData = (data) => {
   console.log(data);
   const cardContainer = document.getElementById("card-container");
-  data.forEach((item) => {
-    const card = document.createElement("div");
-    card.classList = "card w-76  shadow-xl";
-    card.innerHTML = `
-        <figure><img class='h-full' src=${item.thumbnail} alt="Shoes" /></figure>
-        <div class="card-body">
-        <div class="flex gap-5">
-        <img class="w-1/6 rounded-full" src=${item.authors[0].profile_picture}>
-        <h2 class="card-title font-bold">${item.title}</h2>
-        </div>
-        <p class="text-2xl text-gray-700">${
-          item.authors[0].profile_name
-        }<span class="text-blue-400 ml-2">${
-      item.authors[0].verified ? '<i class="fa-solid fa-certificate"></i>' : ""
-    }</span>
-        </P>
-        <P class="text-gray-700">${item.others.views} views</P>
-        </div>
-        `;
-    cardContainer.appendChild(card);
-  });
+  cardContainer.textContent = '';
+  const container = document.getElementById('no-content')
+  if(data.length > 0) {
+    data.forEach((item) => {
+      const card = document.createElement("div");
+      card.classList = "card w-76  shadow-xl";
+      card.innerHTML = `
+          <figure><img class='h-full' src=${item.thumbnail} alt="Shoes" /></figure>
+          <div class="card-body">
+          <div class="flex gap-5">
+          <img class="w-1/6 rounded-full" src=${item.authors[0].profile_picture}>
+          <h2 class="card-title font-bold">${item.title}</h2>
+          </div>
+          <p class="text-2xl text-gray-500">${
+            item.authors[0].profile_name
+          }<span class="text-blue-400 ml-2">${
+        item.authors[0].verified ? '<i class="fa-solid fa-certificate"></i>' : ""
+      }</span>
+          </P>
+          <P class="text-gray-500">${item.others.views} views</P>
+          </div>
+          `;
+      cardContainer.appendChild(card);
+      container.classList.add('hidden');
+    });
+  }
+  else{
+    container.classList.remove('hidden');
+    container.textContent = '';
+    const div = document.createElement('div');
+    div.innerHTML = `
+        <img class="ml-14" src="/assets/images/Icon.png">
+        <p class="font-bold mt-5 text-center">Oops!!Sorry,There is no content here</p>
+    `
+    container.appendChild(div)
+  }
 };
 
+
 loadAllButton()
-loadAllData();
+
+
